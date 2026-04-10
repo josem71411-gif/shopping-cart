f12f21aec3643c14675cdfbdad12121373b0edee
josem71411-gif <josem71411@gmail.com>
2026-04-10 10:50:17 -0500

    Shopping exercise

---
+++ src/main/java/com/plurasight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.plurasight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("pineapple, dog food, apples, pears, grapes, milk, strawberries, water, eggs, tortillas");
+    }
+}
